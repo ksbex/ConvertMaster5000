ccb2d22ba9b30bff53a94b0a5b092fae74b4b71c
Kevin Becker <kevin@kevinbecker.org>
2021-06-18 19:29:12 -0400

    Initial Commit

---
+++ ConvertMaster5000/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConvertMaster5000
+//
+//  Created by Kevin Becker on 6/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ConvertMaster5000/ConvertMaster5000App.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertMaster5000App.swift
+//  ConvertMaster5000
+//
+//  Created by Kevin Becker on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ConvertMaster5000App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
