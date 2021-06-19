c9f972a3e10afad8a1df33f0e0d9992e4f840b6d
Kevin Becker <kevin@kevinbecker.org>
2021-06-18 20:18:39 -0400

    Day 18 challenge complete

--- ConvertMaster5000/ContentView.swift
+++ ConvertMaster5000/ContentView.swift
@@ -8,9 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputTemp = ""
+    @State private var inputUnit = 0
+    @State private var outputUnit = 0
+    
+    let tempUnits = ["Kelvin","Celsius","Fahrenheit"]
+    
+    var kelvinTemp: Double {
+        let temp = Double(inputTemp) ?? 0
+        switch inputUnit {
+        case 0:
+            //kelvin to kelvin
+            return temp
+        case 1:
+            //celcius to kelvin
+            return temp + 273.15
+        case 2:
+            //fahrenheit to kelvin
+            return (temp + 459.67) * 5 / 9
+        
+        default:
+            return 0
+        }
+    }
+    
+    var convertedTemp: Double {
+        switch outputUnit {
+        case 0:
+            //output kelvin
+            return kelvinTemp
+        case 1:
+            //output celcius
+            return kelvinTemp - 273.15
+        case 2:
+            //output fahrenheit
+            return kelvinTemp * 1.8 - 459.67
+        
+        default:
+            return 0
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section(header: Text("Temperature to Convert")){
+                TextField("Temperature", text: $inputTemp)
+                    .keyboardType(.decimalPad)
+                Picker("Units", selection: $inputUnit) {
+                    ForEach(0 ..< tempUnits.count) {
+                        Text("\(self.tempUnits[$0])")
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            Section(header: Text("Converted Temperature")){
+                Text("\(convertedTemp, specifier: "%.1f") °")
+            }
+            Picker("Units", selection: $outputUnit){
+                ForEach(0 ..< tempUnits.count) {
+                    Text("\(self.tempUnits[$0])")
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+        }
     }
 }
 
